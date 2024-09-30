c234df314f78d36dab2f6ba9cd95bbe437cd32eb
daye200 <daye200@naver.com>
2024-09-30 11:23:19 +0900

    feat(수정):수정

--- src/main/java/com/example/springhw1/controller/UserController.java
+++ src/main/java/com/example/springhw1/controller/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,10 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     @Autowired
-    public UserController(UserService userService){this.userService =userService; }
+    public UserController(UserService userService){this.userService = userService; }
     @GetMapping("/user/join")
-    public void join(){
-        userService.saveUser("이다예","1234","힘들다");
+    public User join(@RequestParam("username") String username,@RequestParam("password")String password,@RequestParam("nickname")String nickname){
+        return userService.saveUser(username,password,nickname);
     }
     @GetMapping("user/endpoint/{username}")
     public User find(@PathVariable("username")String name){return userService.findUserByUsername(name);}
